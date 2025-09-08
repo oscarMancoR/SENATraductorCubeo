e2d38b094bef6ae66ce18204c6bbc462556e7d40
Oscar Manco <o.manco@uniandes.edu.co>
2025-09-07 21:46:17 -0500

    CREACION

--- app/src/main/java/com/sena/sennova/cubeoTranslator/PrincipalPage/UI/View/Vista2.kt
+++ app/src/main/java/com/sena/sennova/cubeoTranslator/PrincipalPage/UI/View/Vista2.kt
@@ -1,6 +1,6 @@
 package com.sena.sennova.cubeoTranslator.PrincipalPage.UI.View
 
-import android.widget.EditText
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,7 +19,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
+
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
