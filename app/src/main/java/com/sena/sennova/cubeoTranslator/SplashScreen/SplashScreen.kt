fe32a4afb27d387545719e022d8499959d56318f
Oscar Manco <o.manco@uniandes.edu.co>
2025-09-07 21:46:17 -0500

    CREACION

--- app/src/main/java/com/sena/sennova/cubeoTranslator/SplashScreen/SplashScreen.kt
+++ app/src/main/java/com/sena/sennova/cubeoTranslator/SplashScreen/SplashScreen.kt
@@ -1,9 +1,9 @@
 package com.sena.sennova.cubeoTranslator.SplashScreen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,7 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
